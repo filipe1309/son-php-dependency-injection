24e2284b7efeb21f4b6e12be136ee12f005483c7
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-04-19 12:50:37 -0300

    chore: add another db driver in ex2

--- 01-intro/example_2_interfaces.php
+++ 01-intro/example_2_interfaces.php
@@ -18,6 +18,19 @@ class PdoDriver implements DatabaseDriver
     }
 }
 
+class MongoDbDriver implements DatabaseDriver
+{
+    public function configure(array $config)
+    {
+        $this->config = $config;
+    }
+
+    public function connect()
+    {
+        $mongo = new \MongoClient($this->config['server']);
+    }
+}
+
 class Database
 {
     public function __construct(DatabaseDriver $driver)
@@ -33,4 +46,11 @@ $ioc['db'] = function () {
     return new Database($pdoDriver);
 };
 
+$ioc['db_mongo'] = function () {
+    $mongoDriver = new MongoDbDriver();
+    $mongoDriver->configure([]);
+    return new Database($mongoDriver);
+};
+
 $ioc['db']();
+$ioc['db_mongo']();
