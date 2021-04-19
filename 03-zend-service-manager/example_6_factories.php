34694b46b1eeea6bd26703473695e0dacc858137
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-04-19 20:45:50 -0300

    chore: add factory service manager ex

---
+++ 03-zend-service-manager/example_6_factories.php
@@ -0,0 +1,58 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\ServiceManager;
+
+class TestAdapter
+{
+    public function __construct()
+    {
+        var_dump(TestAdapter::class . '::__construct()');
+    }
+
+    public function runTest($message)
+    {
+        var_dump($message);
+    }
+}
+
+class Tester
+{
+    public function __construct(TestAdapter $adapter)
+    {
+        $adapter->runTest('Test run!!!');
+    }
+}
+
+// Factory class
+class TestAdapterFactory
+{
+    public function __invoke()
+    {
+        // Create an instance of the class.
+        return new TestAdapter;
+    }
+}
+
+class TesterFactory
+{
+    public function __invoke(
+        ContainerInterface $container
+    ) {
+        // Create an instance of the class.
+        return new Tester($container->get('ta'));
+    }
+}
+
+
+$serviceManager = new ServiceManager();
+$serviceManager->setFactory('ta', TestAdapterFactory::class);
+$serviceManager->setFactory('tester', TesterFactory::class);
+
+
+$object1 = $serviceManager->get('ta');
+$object2 = $serviceManager->get('tester');
+
+var_dump($object1, $object2);
