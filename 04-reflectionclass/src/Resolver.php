755c62c314ae39f994d760f7f654b28f49b37977
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-04-21 16:19:40 -0300

    feat: add function di to resolver

--- 04-reflectionclass/src/Resolver.php
+++ 04-reflectionclass/src/Resolver.php
@@ -6,6 +6,20 @@ class Resolver
 {
     private $dependencies_inject;
 
+    public function resolveFunction($fn, $dependencies_inject = [])
+    {
+        if ($dependencies_inject !== []) {
+            $this->dependencies_inject = $dependencies_inject;
+        }
+
+        $info = new \ReflectionFunction($fn);
+
+        $parameters = $info->getParameters();
+        $dependencies = $this->getDependencies($parameters);
+
+        return call_user_func_array($info->getClosure(), $dependencies);
+    }
+
     public function resolveClass($class, $dependencies_inject = [])
     {
         if ($dependencies_inject !== []) {
